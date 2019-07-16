179e08a345afa4a678d185e95f0a21745e54e225
MercurialAnt <mata.anthony98@gmail.com>
2019-07-16 13:51:18 -0700

    Added one of the api calls but need to discuss what to add to it. I have tested it though and it works.

--- app/src/main/java/com/example/ibuprofen/MainActivity.java
+++ app/src/main/java/com/example/ibuprofen/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.ibuprofen;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.ibuprofen.Toolbar.EventFragment;
