cd81f78a8c39ce42a68fbef2f7d20eba59cc5d0b
MercurialAnt <mata.anthony98@gmail.com>
2019-07-16 11:51:19 -0700

    I added the login activity and changed the manifest to get some of the permission we will need for the future. Also changed the manifest to go to the login activity and added an application for parse. I changed the nav bar icons in order to test my changes, but those are not final

--- app/src/main/java/com/example/ibuprofen/HomeActivity.java
+++ app/src/main/java/com/example/ibuprofen/HomeActivity.java
@@ -1,13 +1,83 @@
 package com.example.ibuprofen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // Instance variables of the views
+    private EditText username_et;
+    private EditText password_et;
+    private Button login_btn;
+    private Button signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+    if (ParseUser.getCurrentUser() != null) {
+        to_landing_page();
     }
+
+        username_et = findViewById(R.id.username_et);
+        password_et = findViewById(R.id.password_et);
+        login_btn = findViewById(R.id.login_btn);
+        signup_btn = findViewById(R.id.signup_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = username_et.getText().toString();
+                final String password = password_et.getText().toString();
+
+                login(username, password);
+            }
+        });
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void to_landing_page() {
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("Login Activity", "Login successful");
+                    final Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.e("Login Activity", "Login error");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
 }
