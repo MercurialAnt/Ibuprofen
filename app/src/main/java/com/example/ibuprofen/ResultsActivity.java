a7b1391175b93bed44a2380420856d28afbf7d38
MercurialAnt <mata.anthony98@gmail.com>
2019-07-19 17:56:57 -0700

    merging

---
+++ app/src/main/java/com/example/ibuprofen/ResultsActivity.java
@@ -0,0 +1,69 @@
+package com.example.ibuprofen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ibuprofen.model.Restaurant;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    List<Restaurant> restaurants;
+    ResultsAdapter resultsAdapter;
+    RecyclerView result_rv;
+    Button done_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        result_rv = findViewById(R.id.results_rv);
+        done_btn = findViewById(R.id.done_btn);
+
+        restaurants = new ArrayList<>();
+        Intent intent = getIntent();
+        JSONArray places;
+        String jsonResults = intent.getStringExtra("votedOn");
+        try {
+            places = new JSONArray(jsonResults);
+            for (int i = 0; i < places.length(); i++) {
+                restaurants.add(Restaurant.fromJSON(places.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        resultsAdapter = new ResultsAdapter(this, restaurants);
+
+        result_rv.setAdapter(resultsAdapter);
+        result_rv.setLayoutManager(new LinearLayoutManager(this));
+
+       done_btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent finishedIntent = new Intent(ResultsActivity.this, MainActivity.class);
+               startActivity(finishedIntent);
+               finish();
+           }
+       });
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/ibuprofen/ResultsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.ibuprofen;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ibuprofen.model.Restaurant;
+
+import java.util.List;
+
+public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHolder> {
+    List<Restaurant> restaurants;
+    Context context;
+
+    public ResultsAdapter(Context context, List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_result, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Restaurant restaurant = restaurants.get(position);
+        viewHolder.bind(restaurant);
+    }
+
+    @Override
+    public int getItemCount() {
+        return restaurants.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView count_tv;
+        private TextView name_tv;
+
+        public ViewHolder(@NonNull View itemView) {
+           super(itemView);
+            count_tv = itemView.findViewById(R.id.count_tv);
+            name_tv = itemView.findViewById(R.id.name_tv);
+
+        }
+
+        public void bind(Restaurant restaurant) {
+            count_tv.setText(String.format("%d", restaurant.getCount()));
+            name_tv.setText(restaurant.getName());
+        }
+    }
+}
