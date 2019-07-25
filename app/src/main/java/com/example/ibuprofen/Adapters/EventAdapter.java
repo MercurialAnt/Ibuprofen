d38592efa0df17349468eb5e911a8cc35d5819bb
Amber Patrick <amtpatrick@gmail.com>
2019-07-24 18:06:12 -0700

    Creator name and photo appears for each event

--- app/src/main/java/com/example/ibuprofen/Adapters/EventAdapter.java
+++ app/src/main/java/com/example/ibuprofen/Adapters/EventAdapter.java
@@ -3,23 +3,34 @@ package com.example.ibuprofen.Adapters;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.ibuprofen.ChooseActivity;
 import com.example.ibuprofen.DetailsActivity;
 import com.example.ibuprofen.R;
 import com.example.ibuprofen.ResultsActivity;
 import com.example.ibuprofen.model.Event;
+import com.example.ibuprofen.model.Restaurant;
 import com.parse.CountCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -48,7 +59,11 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         Event event = events.get(position);
-        viewHolder.bind(event);
+        try {
+            viewHolder.bind(event);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -75,11 +90,22 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
             view.setOnClickListener(this);
         }
 
-        public void bind(Event event) {
+        public void bind(Event event) throws ParseException {
             // set username
-            tvCreator.setText(user.getUsername());
-
+            tvCreator.setText(event.getCreator().fetchIfNeeded().getUsername());
             // set image (either restaurant of choice or profile picture of organizer)
+            ParseFile creatorImage = (ParseFile) event.getCreator().fetchIfNeeded().get("profilePic");
+            creatorImage.getDataInBackground(new GetDataCallback() {
+                public void done(byte[] data, ParseException e) {
+                    if (e == null) {
+                        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        // ImageView
+                        ivRestaurant.setImageBitmap(bmp);
+                    } else {
+                        Log.d("test", "Problem load image the data.");
+                    }
+                }
+            });
 
             // find number of event attendees (list all of them in details page)
             ParseRelation<ParseUser> members = event.getMembers();
