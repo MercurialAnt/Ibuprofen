b5bcbfc11f9bcbb3aac77050c55755856023097d
Amber Patrick <amtpatrick@gmail.com>
2019-07-31 10:58:42 -0700

    Preliminary badge update

--- app/src/main/java/com/example/ibuprofen/MainActivity.java
+++ app/src/main/java/com/example/ibuprofen/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.ibuprofen;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.ibuprofen.Toolbar.EventNameFragment;
 import com.example.ibuprofen.Toolbar.FeedFragment;
@@ -17,6 +21,7 @@ import com.example.ibuprofen.Toolbar.ProfileFragment;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private View notificationBadge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         final FragmentManager fragmentManager = getSupportFragmentManager();
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
+        addBadgeView();
 
         // hide support action bar
         getSupportActionBar().hide();
@@ -55,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
 
         //set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_feed);
+    }
+
+    public void addBadgeView() {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+        BottomNavigationItemView itemView = (BottomNavigationItemView) menuView.getChildAt(0);
+
+        notificationBadge = LayoutInflater.from(this).inflate(R.layout.view_notification_badge, menuView, false);
+
+        itemView.addView(notificationBadge);
     }
 }
