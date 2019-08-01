c1e4f4cf4ecf0e206fb199d529d9e5d57433bde7
MercurialAnt <mata.anthony98@gmail.com>
2019-08-01 06:35:22 -0700

    Added the ability for the recycler view of people to not appear only if clicked by the third one

--- app/src/main/java/com/example/ibuprofen/Adapters/ResultsAdapter.java
+++ app/src/main/java/com/example/ibuprofen/Adapters/ResultsAdapter.java
@@ -2,18 +2,25 @@ package com.example.ibuprofen.Adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.ibuprofen.R;
 import com.example.ibuprofen.model.Restaurant;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -50,6 +57,7 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHold
 
         private TextView tvCount;
         private TextView tvName;
+        private LinearLayout llProfiles;
         private RecyclerView rvPeople;
         private List<ParseUser> people;
         private PeopleAdapter peopleAdapter;
@@ -58,6 +66,7 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHold
            super(itemView);
             tvCount = itemView.findViewById(R.id.tvCount);
             tvName = itemView.findViewById(R.id.tvName);
+            llProfiles = itemView.findViewById(R.id.llProfiles);
 
             people = new ArrayList<>();
             rvPeople = itemView.findViewById(R.id.rvPeople);
@@ -74,6 +83,7 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHold
                     if (e == null) {
                         addPeople(objects);
                         tvCount.setText(String.format("%d", objects.size()));
+                        prepare_people(objects);
                     } else {
                         e.printStackTrace();
                     }
@@ -88,6 +98,58 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHold
                 people.add(list.get(i));
             }
             peopleAdapter.notifyDataSetChanged();
+        }
+
+        private void prepare_people(final List<ParseUser> users) {
+            int size = users.size();
+            if (size == 0) {
+                return;
+            }
+            int cutoff = Math.min(size, 2);
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 80);
+            layoutParams.setMargins(2, 2, 2, 0);
+            ImageView[] imageViews = new ImageView[cutoff];
+
+            for (int i = 0; i < cutoff; i++) {
+                imageViews[i] = new ImageView(context);
+                loadPic(imageViews[i], users.get(i));
+                llProfiles.addView(imageViews[i], i, layoutParams);
+            }
+            if (size > cutoff) {
+                ImageView extra = new ImageView(context);
+                extra.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.plusoptions));
+                extra.setOnClickListener(new View.OnClickListener() {
+                    boolean clicked = true;
+                    @Override
+                    public void onClick(View v) {
+                        if (clicked) {
+                            rvPeople.setVisibility(View.VISIBLE);
+                            clicked = false;
+                        } else {
+                            rvPeople.setVisibility(View.GONE);
+                            clicked = true;
+                        }
+                    }
+                });
+                llProfiles.addView(extra, 2, layoutParams);
+            }
+        }
+
+        public void loadPic(ImageView iv, ParseUser user) {
+            RequestOptions options = new RequestOptions()
+                    .error(R.drawable.ic_launcher_background)
+                    .transform(new CircleCrop());
+
+            ParseFile file = user.getParseFile("profilePic");
+            String url = "";
+            if (file != null) {
+                url = file.getUrl();
+            }
+            Glide.with(context)
+                    .load(url)
+                    .apply(options)
+                    .into(iv);
         }
     }
 }
