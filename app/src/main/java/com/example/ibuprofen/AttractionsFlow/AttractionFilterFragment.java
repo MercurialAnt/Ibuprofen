ccbc0f86651ca40c765daa9e3e5654ff2988c9e7
MercurialAnt <mata.anthony98@gmail.com>
2019-08-10 13:18:01 -0700

    added the base fragment so people can add stuff

---
+++ app/src/main/java/com/example/ibuprofen/AttractionsFlow/AttractionFilterFragment.java
@@ -0,0 +1,40 @@
+package com.example.ibuprofen.AttractionsFlow;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ibuprofen.R;
+
+public class AttractionFilterFragment extends Fragment {
+
+    private Activity mActivity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof Activity) {
+            mActivity = (Activity) context;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.filter_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+
+
+}
